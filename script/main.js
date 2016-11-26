ae6392da185daeebd23c3e6631f2e1f0d21dc602
klawgolfer7 <kevinrlawler@gmail.com>
2016-11-26 11:22:20 -0500

    Set up links in HTML and added config function in main.js

---
+++ script/main.js
@@ -0,0 +1,18 @@
+document.ready(fucntion() {
+	// preparing the Document and clearing everything to start the Javascript
+
+	function config() {
+		availableLetters = 'abcdefghijklmnopqrstuvwxyz';
+		lives = 8;
+		words = ["basketball", "baseball", "golf", "football", "birdie",
+				"foul", "penalty", "strike", "slam dunk", "home run", "double play"];
+		messages = {
+			win: 'You win!',
+			lose: 'Game over!',
+			guessed: ' already has been guessed, please try again.',
+			validLetter: 'Please enter a letter from A-Z'
+		};
+	}
+
+
+});
