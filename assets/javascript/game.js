b2014c27154e993c8fd928b47288a6aed2837f67
Kevin Lawler <kevinrlawler@gmail.com>
2016-11-26 21:01:38 -0500

    Fine tuning Javascript.  Changing ID names in HTML tto make more sense.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,7 +14,7 @@ var guesses = [
 var random = Math.floor((Math.random()*(guesses.length-1))); 
 var randomsportword = guesses[random]; // the word to guess will be chosen from the array above
 var placedletter = new Array(randomsportword.length);
-var fehler = 0;
+var startletter = 0;
 
 // every letter in the word is symbolized by an underscore in the guessfield
 for (var i = 0; i < placedletter.length; i++){
@@ -24,54 +24,54 @@ for (var i = 0; i < placedletter.length; i++){
 // prints the guessfield
 function printsportword(){
 	for (var i = 0; i < placedletter.length; i++){
-	var ratefeld = document.getElementById("ratefeld");
+	var guessfield = document.getElementById("underlinefield");
 	var buchstabe = document.createTextNode(placedletter[i]);
-	ratefeld.appendChild(buchstabe);
+	guessfield.appendChild(buchstabe);
 	}
 }
 
 //checks if the the letter provided by the user matches one or more of the letters in the word
-var pruefeZeichen = function(){
+var checkForMatch = function(){
 	var f = document.rateformular; 
-	var b = f.elements["ratezeichen"]; 
-	var zeichen = b.value; // the letter provided by the user
-	zeichen = zeichen.toUpperCase();
+	var b = f.elements["letterinput"]; 
+	var userInput = b.value; // the letter provided by the user
+	userInput = userInput.toUpperCase();
 	for (var i = 0; i < randomsportword.length; i++){
-		if(randomsportword[i] === zeichen){
-			placedletter[i] = zeichen + " ";
-			var treffer = true;
+		if(randomsportword[i] === userInput){
+			placedletter[i] = userInput + " ";
+			var response = true;
 		}
 	b.value = "";
 	}
 	
 	//deletes the guessfield and replaces it with the new one
-	var ratefeld = document.getElementById("ratefeld");
-	ratefeld.innerHTML=""; 
+	var guessfield = document.getElementById("underlinefield");
+	guessfield.innerHTML=""; 
 	printsportword();
 	
 	// if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list and hangman grows
-	if(!treffer){
-		var gerateneBuchstaben = document.getElementById("gerateneBuchstaben");
-		var buchstabe = document.createTextNode(" " + zeichen);
-		gerateneBuchstaben.appendChild(buchstabe); 
-		fehler++;
+	if(!response){
+		var wrongLetter = document.getElementById("wrongLetter");
+		var buchstabe = document.createTextNode(" " + userInput);
+		wrongLetter.appendChild(buchstabe); 
+		startletter++;
 		var hangman = document.getElementById("hangman");
-    hangman.src = "http://www.writteninpencil.de/Projekte/Hangman/hangman" + fehler + ".png";
+    hangman.src = "http://www.writteninpencil.de/Projekte/Hangman/hangman" + startletter + ".png";
 	}
 	
 	//checks if all letters have been found
-	var fertig = true;
+	var checkWin = true;
 	for (var i = 0; i < placedletter.length; i++){
 		if(placedletter[i] === "_ "){
-			fertig = false;
+			checkWin = false;
 		}
 	}
-	if(fertig){
+	if(checkWin){
 		window.alert("You win!");
 	}
 	
 	//once you got six wrong letters, you lose
-	if(fehler === 6){
+	if(startletter === 6){
 		window.alert("Uh...I guess you're dead now.");
 	}
 }
